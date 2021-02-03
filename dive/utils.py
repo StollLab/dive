0ac9db74ab480937ca3bde23f135cbfea8946d66
Stephan Pribitzer <stephapr@uw.edu>
2021-02-02 23:47:29 -0800

    add crucial utility functions

--- dive/utils.py
+++ dive/utils.py
@@ -1,8 +1,13 @@
 import numpy as np
 import math as m
+import sys
 from scipy.special import fresnel
+import pymc3 as pm
+from datetime import date
+import os   
 
 from .constants import *
+from .deerload import*
 
 def addnoise(V,sig):
     """
@@ -38,3 +43,72 @@ def dipolarkernel(t,r):
         K = K*dr
     
     return K
+
+def loadTrace(FileName):
+    """
+    Load a DEER trace, can be a bruker or comma separated file.
+    """
+    if FileName.endswith('.dat') or FileName.endswith('.txt') or FileName.endswith('.csv'):
+        data = np.genfromtxt(FileName,delimiter=',',skip_header=1)
+        t = data[:,0]
+        Vdata = data[:,1]
+    elif FileName.endswith('.dat'):
+        t, Vdata, Parameters = deerload(FileName)
+    else:
+        sys.exit('The file format is not recognized.')
+
+    return t, Vdata
+
+def sample(Model,MCMCparameters):
+    """ 
+    Use pymc3 to draw samples from the posterior for the model, according to the parameters provided with MCMCparameters.
+    """
+    RequiredKeys = ["draws","tune","chains"]
+
+    for Key in RequiredKeys:
+        if Key not in MCMCparameters:
+            sys.exit("{} is a required MCMC parameter".format(Key))            
+
+    if "cores" not in MCMCparameters:
+        MCMCparameters["cores"] = 2
+    
+    # Sampling
+    trace = pm.sample(model=Model, draws=MCMCparameters["draws"], tune=MCMCparameters["tune"], chains=MCMCparameters["chains"],cores=MCMCparameters["cores"])
+
+    # # Result processing 
+    df = pm.trace_to_dataframe(trace)
+
+    return df
+
+def saveTrace(df,Parameters,SaveName='empty'):
+    
+    if SaveName == 'empty':
+        today = date.today()
+        datestring = today.strftime("%Y%m%d")
+        SaveName = "./traces/{}_traces.dat".format(datestring)
+    
+    if not SaveName.endswith('.dat'):
+        SaveName = SaveName+'.dat'
+
+    shape = df.shape 
+    cols = df.columns.tolist()
+
+    os.makedirs(os.path.dirname(SaveName), exist_ok=True)
+
+    f = open(SaveName, 'a+')
+    f.write("# Traces from the MCMC simulations with pymc3\n")
+    f.write("# The following {} parameters were investigated:\n".format(shape[1]))
+    f.write("# {}\n".format(cols))
+    f.write("# nParameters nChains nIterations\n")
+    if Parameters['nGauss'] == 1:
+        f.write("{},{},{},0,0,0\n".format(shape[1],Parameters['chains'],Parameters['draws']))
+    elif Parameters['nGauss'] == 2:
+        f.write("{},{},{},0,0,0,0,0,0,0,0,0,0\n".format(shape[1],Parameters['chains'],Parameters['draws']))
+    elif Parameters['nGauss'] == 3:
+        f.write("{},{},{},0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n".format(shape[1],Parameters['chains'],Parameters['draws']))
+    elif Parameters['nGauss'] == 4:
+        f.write("{},{},{},0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n".format(shape[1],Parameters['chains'],Parameters['draws']))
+
+    df.to_csv (f, index=False, header=False)
+
+    f.close()
