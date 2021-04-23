68f0ea774d50982c4cfa84abf6af9168e5ca5dd8
Stephan Pribitzer <stephapr@uw.edu>
2021-04-23 13:02:07 -0700

    add Bardsley/Edwards sample for P

---
+++ dive/samplers.py
@@ -0,0 +1,272 @@
+import numpy as np
+import math as m
+from theano import tensor as T
+from theano.tensor import nlinalg as tnp
+from theano.tensor import slinalg as snp
+import pymc3 as pm
+
+def randP(delta,tauKtK,tauKtS,LtL,nr):
+    r"""
+    based on:
+    J.M. Bardsley, C. Fox, An MCMC method for uncertainty quantification in
+    nonnegativity constrained inverse problems, Inverse Probl. Sci. Eng. 20 (2012)
+    """
+    invSigma = tauKtK + delta*LtL
+    Sigma = np.linalg.inv(invSigma)
+
+    try:
+        #'lower' syntax is faster for sparse matrices. Also matches convention in
+        # Bardsley paper.
+        C_L = np.linalg.cholesky(Sigma)
+    except:
+        C_L = np.linalg.sqrtm(Sigma)
+        
+    v = np.random.standard_normal(size=(nr,))
+    w = np.linalg.lstsq(np.matrix.transpose(C_L),v,rcond=None)
+    w = w[0]
+
+    P = fnnls(invSigma,tauKtS+w)
+
+    return P
+
+def randP_p(delta,tauKtK,tauKtS,LtL,nr):
+    r"""
+    The pymc3 compatible version of randP, based on:
+    J.M. Bardsley, C. Fox, An MCMC method for uncertainty quantification in
+    nonnegativity constrained inverse problems, Inverse Probl. Sci. Eng. 20 (2012)
+    """
+    invSigma = tauKtK + delta*LtL
+    Sigma = tnp.matrix_inverse(invSigma)
+
+    try:
+        #'lower' syntax is faster for sparse matrices. Also matches convention in
+        # Bardsley paper.
+        C_L = snp.cholesky(Sigma)
+        # C_L = np.linalg.cholesky(Sigma)
+    except:
+        C_L = tnp.sqrtm(Sigma)
+        
+    v = np.random.standard_normal(size=(nr,))
+    # w = tnp.lstsq()(T.transpose(C_L),v,rcond=None)
+    w = snp.solve(T.transpose(C_L),v)
+    # w = w[0]
+
+    P = fnnls_T(invSigma,tauKtS+w,nr)
+
+    return P     
+
+
+def fnnls_T(AtA,Atb,nr,tol=[],maxiter=[],verbose=False):
+#=====================================================================================
+    r"""
+    FNNLS   Fast non-negative least-squares algorithm.
+    x = fnnls(AtA,Atb) solves the problem min ||b - Ax|| if
+        AtA = A'*A and Atb = A'*b.
+    A default tolerance of TOL = MAX(SIZE(AtA)) * NORM(AtA,1) * EPS
+    is used for deciding when elements of x are less than zero.
+    This can be overridden with x = fnnls(AtA,Atb,TOL).
+
+    [x,w] = fnnls(AtA,Atb) also returns dual vector w where
+        w(i) < 0 where x(i) = 0 and w(i) = 0 where x(i) > 0.
+    
+    For the FNNLS algorithm, see
+        R. Bro, S. De Jong
+        A Fast Non-Negativity-Constrained Least Squares Algorithm
+        Journal of Chemometrics 11 (1997) 393-401
+    The algorithm FNNLS is based on is from
+        Lawson and Hanson, "Solving Least Squares Problems", Prentice-Hall, 1974.
+    """
+
+    unsolvable = False
+    count = 0
+
+    # Use all-zero starting vector
+    N = nr
+    x = np.zeros(nr)
+
+    # Calculate tolerance and maxiter if not given.
+    if np.size(np.atleast_1d(tol))==0:
+        eps = np.finfo(float).eps
+        tol = 10*eps*tnp.norm(AtA,1)*N
+    if np.size(np.atleast_1d(maxiter))==0:
+        maxiter = 5*N
+
+
+    passive = x>0       # initial positive/passive set (points where constraint is not active)
+    x[~passive] = 0
+    w = Atb - pm.math.dot(AtA,x)     # negative gradient of error functional 0.5*||A*x-y||^2
+    
+    # Outer loop: Add variables to positive set if w indicates that fit can be improved.
+    outIteration = 0
+    maxIterations = 5*N    
+    while T.any(pm.math.lt(w,tol)) and np.any(~passive):
+        outIteration += 1
+        
+        # Add the most promising variable (with largest w) to positive set.
+        t = np.argmax(w)
+        passive[t] = True
+        
+        # Solve unconstrained problem for new augmented positive set.
+        # This gives a candidate solution with potentially new negative variables.
+        x_ = np.zeros(N)
+        if np.sum(passive)==1:
+            x_[passive] = Atb[passive]/AtA[passive,passive]
+        else:
+            x_[passive] = np.linalg.solve(AtA[np.ix_(passive,passive)],Atb[passive])
+        
+        # Inner loop: Iteratively eliminate negative variables from candidate solution.
+        iIteration = 0
+        while any((x_<=tol) & passive) and iIteration<maxIterations:
+            iIteration += 1
+            
+            # Calculate maximum feasible step size and do step.
+            negative = (x_<=tol) & passive
+            alpha = min(x[negative]/(x[negative]-x_[negative]))
+            x += alpha*(x_-x)
+            
+            # Remove all negative variables from positive set.
+            passive[x<tol] = False
+            
+            # Solve unconstrained problem for reduced positive set.
+            x_ = np.zeros(N)
+            if np.sum(passive)==1:
+                x_[passive] = Atb[passive]/AtA[passive,passive]
+            else:
+                x_[passive] = np.linalg.solve(AtA[np.ix_(passive,passive)],Atb[passive])
+            
+        # Accept non-negative candidate solution and calculate w.
+        if all(x == x_):
+            count += 1
+        else:
+            count = 0
+        if count > 5:
+            unsolvable = True
+            break
+        x = x_
+        
+        w = Atb - AtA@x
+        w[passive] = -m.inf
+        if verbose:
+            print('{:10.0f}{:15.0f}{:20.4e}\n'.format(outIteration,iIteration,max(w)) )
+
+    if verbose:
+        if unsolvable:
+            print('Optimization stopped because the solution cannot be further changed. \n')
+        elif any(~passive):
+            print('Optimization stopped because the active set has been completely emptied. \n')
+        elif w>tol:
+            print('Optimization stopped because the gradient (w) is inferior than the tolerance value TolFun = #.6e. \n' %tol)
+        else:
+            print('Solution found. \n')
+    
+    return x
+
+#=====================================================================================
+
+def fnnls(AtA,Atb,tol=[],maxiter=[],verbose=False):
+#=====================================================================================
+    r"""
+    FNNLS   Fast non-negative least-squares algorithm.
+    x = fnnls(AtA,Atb) solves the problem min ||b - Ax|| if
+        AtA = A'*A and Atb = A'*b.
+    A default tolerance of TOL = MAX(SIZE(AtA)) * NORM(AtA,1) * EPS
+    is used for deciding when elements of x are less than zero.
+    This can be overridden with x = fnnls(AtA,Atb,TOL).
+
+    [x,w] = fnnls(AtA,Atb) also returns dual vector w where
+        w(i) < 0 where x(i) = 0 and w(i) = 0 where x(i) > 0.
+    
+    For the FNNLS algorithm, see
+        R. Bro, S. De Jong
+        A Fast Non-Negativity-Constrained Least Squares Algorithm
+        Journal of Chemometrics 11 (1997) 393-401
+    The algorithm FNNLS is based on is from
+        Lawson and Hanson, "Solving Least Squares Problems", Prentice-Hall, 1974.
+    """
+
+    unsolvable = False
+    count = 0
+
+    # Use all-zero starting vector
+    N = np.shape(AtA)[1]
+
+    x = np.zeros(N)
+
+    # Calculate tolerance and maxiter if not given.
+    if np.size(np.atleast_1d(tol))==0:
+        eps = np.finfo(float).eps
+        tol = 10*eps*np.linalg.norm(AtA,1)*max(np.shape(AtA))
+    if np.size(np.atleast_1d(maxiter))==0:
+        maxiter = 5*N
+
+
+    passive = x>0       # initial positive/passive set (points where constraint is not active)
+    x[~passive] = 0
+    w = Atb - AtA @ x     # negative gradient of error functional 0.5*||A*x-y||^2
+    
+    # Outer loop: Add variables to positive set if w indicates that fit can be improved.
+    outIteration = 0
+    maxIterations = 5*N    
+    while np.any(w>tol) and np.any(~passive):
+        outIteration += 1
+        
+        # Add the most promising variable (with largest w) to positive set.
+        t = np.argmax(w)
+        passive[t] = True
+        
+        # Solve unconstrained problem for new augmented positive set.
+        # This gives a candidate solution with potentially new negative variables.
+        x_ = np.zeros(N)
+        if np.sum(passive)==1:
+            x_[passive] = Atb[passive]/AtA[passive,passive]
+        else:
+            x_[passive] = np.linalg.solve(AtA[np.ix_(passive,passive)],Atb[passive])
+        
+        # Inner loop: Iteratively eliminate negative variables from candidate solution.
+        iIteration = 0
+        while any((x_<=tol) & passive) and iIteration<maxIterations:
+            iIteration += 1
+            
+            # Calculate maximum feasible step size and do step.
+            negative = (x_<=tol) & passive
+            alpha = min(x[negative]/(x[negative]-x_[negative]))
+            x += alpha*(x_-x)
+            
+            # Remove all negative variables from positive set.
+            passive[x<tol] = False
+            
+            # Solve unconstrained problem for reduced positive set.
+            x_ = np.zeros(N)
+            if np.sum(passive)==1:
+                x_[passive] = Atb[passive]/AtA[passive,passive]
+            else:
+                x_[passive] = np.linalg.solve(AtA[np.ix_(passive,passive)],Atb[passive])
+            
+        # Accept non-negative candidate solution and calculate w.
+        if all(x == x_):
+            count += 1
+        else:
+            count = 0
+        if count > 5:
+            unsolvable = True
+            break
+        x = x_
+        
+        w = Atb - AtA@x
+        w[passive] = -m.inf
+        if verbose:
+            print('{:10.0f}{:15.0f}{:20.4e}\n'.format(outIteration,iIteration,max(w)) )
+
+    if verbose:
+        if unsolvable:
+            print('Optimization stopped because the solution cannot be further changed. \n')
+        elif any(~passive):
+            print('Optimization stopped because the active set has been completely emptied. \n')
+        elif w>tol:
+            print('Optimization stopped because the gradient (w) is inferior than the tolerance value TolFun = #.6e. \n' %tol)
+        else:
+            print('Solution found. \n')
+    
+    return x
+
+#=====================================================================================
