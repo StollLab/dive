e985a3b2ce1764f2440a894eaa29ef76880976c1
ambivalent-synchronicity <junarwhals@gmail.com>
2024-08-07 18:11:53 +0000

    fixed pytests

--- tests/test-traces.py
+++ tests/test-traces.py
@@ -13,7 +13,7 @@ def t():
 def Vexp(t):
     # setting up data
     r = np.linspace(2,7,251) 
-    P = dl.dd_gauss(r,5,0.8)
+    P = dive.dd_gauss(r,5,0.8)
 
     lam = 0.4                       # modulation depth
     B = dl.bg_hom3d(t,0.2,lam)         # background decay
@@ -24,20 +24,23 @@ def Vexp(t):
 def test_running_regularization(t,Vexp):
     r = np.linspace(2,7,50)
     model = dive.model(t,Vexp,method="regularization",r=r)
-    trace = dive.sample(model,draws=100,tune=200,chains=1,random_seed=100)
+    trace = dive.sample(model,draws=10,tune=20,chains=1,random_seed=100,
+                        progressbar=False)
     lamb = trace.posterior.lamb[0][0].values
-    assert lamb == 0.39950062979541884
+    assert lamb == 0.28143793404406964
 
 def test_running_gaussian_1(t,Vexp):
     r = np.linspace(2,7,50)
     model = dive.model(t,Vexp,method="gaussian",r=r)
-    trace = dive.sample(model,draws=100,tune=200,chains=1,random_seed=100)
+    trace = dive.sample(model,draws=10,tune=20,chains=1,random_seed=100,
+                        progressbar=False)
     lamb = trace.posterior.lamb[0][0].values
-    assert lamb == 0.4011008024924596
+    assert lamb == 0.35242692781607926
 
 def test_running_gaussian_2(t,Vexp):
     r = np.linspace(2,7,50)
     model = dive.model(t,Vexp,method="gaussian",r=r,n_gauss=2)
-    trace = dive.sample(model,draws=100,tune=200,chains=1,random_seed=100)
+    trace = dive.sample(model,draws=10,tune=20,chains=1,random_seed=100,
+                        progressbar=False)
     lamb = trace.posterior.lamb[0][0].values
-    assert lamb == 0.39991417362040915
+    assert lamb == 0.3931032862443532
