9163f23f4ecae7c7a5eaa73403d99f50b5e31fe2
ambivalent-synchronicity <junarwhals@gmail.com>
2024-08-08 07:25:39 +0000

    attempt to use sphinx_rtd_theme pt5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
