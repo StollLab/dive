56c5d0240f9a0e820ad76705e35c272df6c60578
ambivalent-synchronicity <junarwhals@gmail.com>
2024-08-08 00:25:22 +0000

    try sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,6 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'furo'
 ]
 
 templates_path = ['_templates']
@@ -30,5 +29,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
